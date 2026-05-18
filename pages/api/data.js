3f9940fb7f5797f8f76ea7b8d1a43532c45397b4
bowob0067-debug <bowob0067@gmail.com>
2026-05-18 07:44:19 +0700

    Delete pages/api directory

--- pages/api/data.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbxtFrlRNMv0dTIy1KfUm0N0COnhUZ1gEJTSyq1pNpFlKNI7qVWO8QE1WOMm9tmuCYBh/exec"
-  );
-
-  const data = await response.json();
-
-  res.status(200).json(data);
-}
